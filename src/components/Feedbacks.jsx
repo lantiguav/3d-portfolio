a447034c9c880730862c1597d2d76cee7696b809
Victor Lantigua <lantiguav@gmail.com>
2023-08-09 21:41:37 -0400

    fix: removing margin

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -44,7 +44,7 @@ const FeedbackCard = ({
 
 const Feedbacks = () => {
   return (
-    <div className='mt-12 bg-black-100 rounded-[20px]'>
+    <div className='bg-black-100 rounded-[20px]'>
       <div
         className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
         <Heading heading='Testimonials.' subheading='What others say' />
