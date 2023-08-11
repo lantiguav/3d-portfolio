1f5bcecbb20ce5f5169f9fd568679d9867fc4389
Victor Lantigua <lantiguav@gmail.com>
2023-08-10 21:24:56 -0400

    fix: removed comment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,6 @@ const Hero = () => {
           </div>
         </a>
       </div>
-      {/* <div className=' absolute top-0 left-0 right-0 bottom-0 opacity-0 z-10'></div> */}
     </section>
   )
 }
