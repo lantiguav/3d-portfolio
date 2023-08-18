25e56e52be8e9fae56fe4fb416732b96234ef881
Victor Lantigua <lantiguav@gmail.com>
2023-08-17 21:47:19 -0400

    fix: changing analyticsId env variable 8

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
